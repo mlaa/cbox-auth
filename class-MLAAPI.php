cabc9ae7a4b9efb80188ab1a2034b12a8b538287
Jonathan Reeve <jon.reeve@gmail.com>
2014-12-19 14:05:42 -0500

    disable dummy data functions for now

--- class-MLAAPI.php
+++ class-MLAAPI.php
@@ -9,13 +9,13 @@ abstract class MLAAPI {
 	 */
 	public function send_request( $http_method, $base_url, $parameters = array(), $request_body = '' ) {
 
-		if ( $this->debug ) { 
-			_log( "base URL: $base_url" ); 
+		//if ( $this->debug ) { 
+			//_log( "base URL: $base_url" ); 
 			// @todo: abstract out API URL so that we can better access the useful
 			// part of the url, like /members, and pass this to 
 			// get_mock_data. 
-			return $this->get_mock_data( $http_method, $base_url, $parameters, $request_body ); 
-		} 
+			//return $this->get_mock_data( $http_method, $base_url, $parameters, $request_body ); 
+		//} 
 
 		// The `private.php` file contains API passwords.
 		// It populates the variables $api_key and $api_secret.

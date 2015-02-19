0c2b2445a89e6555c46dccac2ae6d1d30dd1252a
Jonathan Reeve <jon.reeve@gmail.com>
2015-02-19 09:39:58 -0500

    require mu-plugins

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,8 @@ if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../cbox-auth.php';
+	require dirname( __FILE__ ) . '/../../../mu-plugins/debug.php';
+	require dirname( __FILE__ ) . '/../customAuth.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 

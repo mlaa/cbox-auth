5002b066ced9fa1eff44f7528f5694440ec0d173
Jonathan Reeve <jon.reeve@gmail.com>
2015-03-16 16:37:13 -0400

    Revert "non-working attempt to load buddypress and dependencies"
    This reverts commit d88305670cead39655a5c4f1947237d9bf5d4b61.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,34 +3,24 @@
 // set a flag so that certain functions know we're running tests. 
 define( 'RUNNING_TESTS', TRUE ); 
 
-$wp_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $wp_tests_dir ) $wp_tests_dir = '/tmp/wordpress-tests-lib';
+$_tests_dir = getenv('WP_TESTS_DIR');
+if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 
-require_once $wp_tests_dir . '/includes/functions.php';
+require_once $_tests_dir . '/includes/functions.php';
 
-$bp_tests_dir = getenv( 'BP_TESTS_DIR' ); 
-if ( ! $bp_tests_dir ) $bp_tests_dir = '/tmp/buddypress/tests/phpunit';
-define( 'BP_TESTS_DIR', $bp_tests_dir ); 
-
-require $wp_tests_dir . '/includes/bootstrap.php';
+define( 'BP_TESTS_DIR', '/home/jreeve/Apps/wp-bp-tests/buddypress-svn/tests/phpunit' ); 
 
 function _manually_load_plugin() {
 	require BP_TESTS_DIR . '/includes/loader.php';
 
-	// this is my debugging file, which ensures that tests won't fail 
-	// if there's a call to `_log()`. 
 	require dirname( __FILE__ ) . '/debug.php';
-
-	// override this class to load mock data
+	// overrides to get mock data
 	require dirname( __FILE__ ) . '/class-MockMLAAPI.php'; 
-
 	// don't get the whole plugin now, just a few classes, because 
 	// to test them individually we feed them mock data above. 
 	require dirname( __FILE__ ) . '/../class-CustomAuthentication.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-_log( 'using BP_TESTS_DIR:', BP_TESTS_DIR ); 
+require $_tests_dir . '/includes/bootstrap.php';
 
-// Requiring this file gives you access to BP_UnitTestCase
-require $bp_tests_dir . '/includes/testcase.php';

b69863aecdc6d879ceb5d965d3f0f70f39fa65ae
Jonathan Reeve <jon.reeve@gmail.com>
2015-03-17 15:53:54 -0400

    dependency path tweaks for Travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,9 +4,7 @@
 define( 'RUNNING_TESTS', TRUE ); 
 
 $_tests_dir = getenv('WP_TESTS_DIR');
-
 if ( ! $_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
-
 require_once $_tests_dir . '/includes/functions.php';
 
 if ( ! getenv( 'BP_TESTS_DIR' ) ) { 
